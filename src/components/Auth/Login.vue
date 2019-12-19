04d7db75c21a957c119beaf706de66227248a5de
Makentosh <ziki_007@ukr.net>
2019-12-19 21:58:36 +0200

    add new page & login validate

---
+++ src/components/Auth/Login.vue
@@ -0,0 +1,81 @@
+<template>
+    <v-container fluid fill-height>
+        <v-layout align-center justify-center>
+            <v-flex xs12 sm8 md4>
+                <v-card class="elevation-12">
+                    <v-toolbar
+                            color="primary"
+                            dark>
+                        <v-toolbar-title>Login form</v-toolbar-title>
+
+                    </v-toolbar>
+                    <v-card-text>
+                        <v-form v-model="valid" ref="form" validation>
+                            <v-text-field
+                                    label="Email"
+                                    name="email"
+                                    prepend-icon="person"
+                                    type="email"
+                                    v-model="email"
+                                    :rules="emailRules"
+                            />
+
+                            <v-text-field
+                                    id="password"
+                                    label="Password"
+                                    name="password"
+                                    prepend-icon="lock"
+                                    type="password"
+                                    v-model="password"
+                                    :counter="6"
+                                    :rules="passwordRules"
+                            />
+                        </v-form>
+                    </v-card-text>
+                    <v-card-actions>
+                        <v-spacer />
+                        <v-btn color="primary"
+                        @click="onSubmit"
+                        :disabled="!valid"
+                        >Login</v-btn>
+                    </v-card-actions>
+                </v-card>
+            </v-flex>
+        </v-layout>
+    </v-container>
+</template>
+
+<script>
+    export default {
+        data () {
+            return {
+                email: '',
+                password: '',
+                valid: false,
+                emailRules: [
+                    v => !!v || 'E-mail is required',
+                    v => /.+@.+/.test(v) || 'E-mail must be valid',
+                ],
+                passwordRules: [
+                    v => !!v || 'Password is required',
+                    v => v.length >= 6 || 'Password must be less than 6 characters',
+                ]
+            }
+        },
+        methods: {
+            onSubmit () {
+                if (this.$refs.form.validate()) {
+                    const user = {
+                        email: this.email,
+                        password: this.password
+                    }
+                    console.log(user)
+                }
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
