7e1894c8c31f3581d7cc0037bc2d3e59d0e03f8a
Makentosh <ziki_007@ukr.net>
2019-12-23 23:23:21 +0200

    add orders page

--- src/components/User/Orders.vue
+++ src/components/User/Orders.vue
@@ -1,8 +1,28 @@
 <template>
     <v-container>
-        <v-layout row>
-            <v-flex xs12>
-                <h1>Orders</h1>
+        <v-layout row wrap>
+            <v-flex xs12 sm6 offset-sm3>
+                <h1 class="text--secondary mb-3">Orders</h1>
+                <v-list two-line subheader>
+                    <v-list-tile avatar
+                    v-for="order in orders" :key="order.id">
+                        <v-list-tile-action>
+                            <v-checkbox
+                                    :input-value="order.done"
+                            @click="markDone(order)"
+                            color="success"></v-checkbox>
+                        </v-list-tile-action>
+                        <v-list-tile-content>
+                            <v-list-tile-title>{{ order.name}}</v-list-tile-title>
+                            <v-list-tile-sub-title>{{ order.phone}}</v-list-tile-sub-title>
+                        </v-list-tile-content>
+                        <v-list-tile-action>
+                            <v-btn class="primary"
+                            :to="'/ad/' + order.adId">Open</v-btn>
+                        </v-list-tile-action>
+                    </v-list-tile>
+
+                </v-list>
             </v-flex>
         </v-layout>
     </v-container>
@@ -11,7 +31,22 @@
 <script>
     export default {
         data () {
-            return {}
+            return {
+                orders: [
+                    {
+                        adId: '123',
+                        name: 'vlad',
+                        phone: '+56-535-345-345',
+                        adId: '145',
+                        done: false
+                    }
+                ]
+            }
+        },
+        methods: {
+            markDone(order) {
+                order.done = true
+            }
         }
     }
 </script>
