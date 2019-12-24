49886403724ad08b556520afaaa59d953ac7051b
Makentosh <ziki_007@ukr.net>
2019-12-24 21:50:53 +0200

    add my list page

--- src/components/Ads/AdList.vue
+++ src/components/Ads/AdList.vue
@@ -2,8 +2,32 @@
 <template>
     <v-container>
         <v-layout row>
-            <v-flex xs12>
-                <h1>Adlist</h1>
+            <v-flex xs12 sm6 offset-md3>
+                <h1 class="text--secondary mb-3">My ads</h1>
+
+                <v-card class="elevation-10 mb-3"
+                v-for="ad in ads" :key="ad.id">
+                    <v-layout row>
+                        <v-flex xs4>
+                            <v-card-media
+                            :src="ad.imageSrc"
+                            height="175">
+
+                            </v-card-media>
+                        </v-flex>
+                        <v-flex xs8>
+                            <v-card-text>
+                                <h2 class="text--primary">{{ ad.title }}</h2>
+                                <p>{{ ad.description }}</p>
+                            </v-card-text>
+                            <v-card-actions>
+                                <v-spacer></v-spacer>
+                                <v-btn class="info"
+                                :to="'/ad/' + ad.id">Open</v-btn>
+                            </v-card-actions>
+                        </v-flex>
+                    </v-layout>
+                </v-card>
             </v-flex>
         </v-layout>
     </v-container>
@@ -12,7 +36,17 @@
 <script>
     export default {
         data () {
-            return {}
+            return {
+                ads: [
+                    {
+                        title: 'First image',
+                        description: 'Hello i`m description',
+                        promo: false,
+                        imageSrc: 'https://images5.alphacoders.com/947/thumb-1920-947824.png',
+                        id: 123
+                    }
+                ]
+            }
         }
     }
 </script>
