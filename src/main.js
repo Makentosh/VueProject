27c19f874285353bf2e41e4590b9b82575b73008
Makentosh <ziki_007@ukr.net>
2019-12-25 21:40:12 +0200

    add firebase auth

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import App from './App'
 import router from './router'
 import store from './store'
 import Vuetify from 'vuetify'
+import * as fb from 'firebase'
 import 'vuetify/dist/vuetify.min.css'
 
 Vue.use(Vuetify)
@@ -17,5 +18,16 @@ new Vue({
   router,
   store,
   components: { App },
-  template: '<App/>'
+  template: '<App/>',
+  created: function () {
+    fb.initializeApp({
+      apiKey: 'AIzaSyAte_uqrKfclV-CqsiP-dk-l4RFOFPSvfo',
+      authDomain: 'vueproject-5f321.firebaseapp.com',
+      databaseURL: 'https://vueproject-5f321.firebaseio.com',
+      projectId: 'vueproject-5f321',
+      messagingSenderId: '1021477827930',
+      storageBucket: 'vueproject-5f321.appspot.com',
+      appId: '1:1021477827930:web:6e9b8ba6c7b7329c022f63'
+    })
+  }
 })
